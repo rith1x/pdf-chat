ff1d509ba800b418bba01ff482a8f8123b6a6312
rith1x <kiruthikkumarmpc24@gmail.com>
2024-10-15 22:30:46 +0530

    latest

--- app/views.py
+++ app/views.py
@@ -1,3 +1,75 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from langchain_community.document_loaders import PyPDFLoader
+import requests
+import os
 
-# Create your views here.
+def showWelcome(request):
+    return render(request,"welcome.html")
+
+
+def handlePdf(request):
+    if request.method == "POST":
+        pdflink = request.POST.get("link")
+        print(f"Received PDF link: {pdflink}")
+        info = ""
+
+        pdf_dir = '/tmp'
+        pdf_file_path = os.path.join(pdf_dir, 'file.pdf')
+
+        if not os.path.exists(pdf_dir):
+            os.makedirs(pdf_dir)
+
+        try:
+            response = requests.get(pdflink)
+            if response.status_code == 200:
+                with open(pdf_file_path, 'wb') as f:
+                    f.write(response.content)
+                print("File successfully downloaded and saved")
+                info = "File successfully downloaded and saved"
+            else:
+                print(f"Failed to download the PDF. Status code: {response.status_code}")
+                info = f"Failed to download the PDF. Status code: {response.status_code}"
+        except requests.exceptions.RequestException as e:
+            print(f"Error occurred: {str(e)}")
+            info = f"Error occurred while downloading the PDF: {str(e)}"
+
+        return render(request, "main.html", {'info': info})
+    
+    return render(request, "main.html", {'info': "No POST request made."})
+
+# def handlePdf(request):
+#     if request.method == "POST":
+#         pdflink = request.POST.get("link")
+#         print(pdflink)
+#         try:
+#             response = requests.get(pdflink)
+#             if response.status_code == 200:
+#                 with open('/tmp/file.pdf', 'wb') as f:
+#                     f.write(response.content)
+#                 print("File successfully downloaded and saved")
+#                 info = "File successfully downloaded and saved"
+#             else:
+#                 print(f"Failed to download the PDF. Status code: {response.status_code}")
+#                 info = (f"Failed to download the PDF. Status code: {response.status_code}")
+#         except requests.exceptions.RequestException as e:
+#             print(f"Error occurred: {str(e)}")
+        
+#         return render(request, "main.html",{'info':info})
+
+
+def handleQuestion(request):
+    if request.method == 'POST':
+        qn = request.POST.get('ques')
+
+        loader = PyPDFLoader('/tmp/file.pdf')
+        docs = loader.load()
+
+        try:
+            result = docs[int(qn)].page_content
+        except (IndexError, ValueError):
+            result = "Invalid question or page number. Please enter a valid page number."
+    
+        resp = {'message': result}
+        return JsonResponse(resp)
+    return JsonResponse({'error':'Error'},status=404)
